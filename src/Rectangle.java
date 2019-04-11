59088d250322bc80fa89f845c13b705fa41c417d
massin2000 <mhamed.arkoubi@gmail.com>
2019-04-11 20:08:35 +0200

    Matti

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -6,6 +6,15 @@ public class Rectangle extends Shape {
 
     @Override
     public void fill() {
+        for (int i = 0; i < SIZEY ; i++) {
+            for (int j = 0; j < SIZEX ; j++) {
+System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        }
+
         // TODO
     }
-}
+
